0f4eba1ce181a5b23409c2c878582f988d700f3d
PokemasterBill <rafsutev@outlook.com>
2026-06-20 00:37:20 +1000

    Included name and age input plus year when 100 years old

--- Character Input.py
+++ Character Input.py
@@ -1,2 +1,10 @@
-print("Hello there.")
-print("General Kenobi.")
+
+name = input("Hi, what is your name? ")
+age = int(input(f"How old are you {name}? "))
+
+current_year = 2026
+age_difference = 100 - age 
+
+final_age = current_year + age_difference
+
+print(f"{name} will be 100 years old in the year {final_age}")
