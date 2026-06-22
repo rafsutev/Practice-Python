dcda50a9af411cf9cd0fa98c608597f76a924c74
PokemasterBill <rafsutev@outlook.com>
2026-06-22 16:04:55 +1000

    Started exercise

---
+++ String Lists.py
@@ -0,0 +1,15 @@
+# https://www.practicepython.org/exercise/2014/03/12/06-string-lists.html
+
+# user_string = input("Please enter a word: ")
+
+#pasta 
+#atsap
+string = "pasta"
+
+empty_string = []
+
+
+
+
+
+
