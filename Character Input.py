07da8849fe4dc5db094075e912edc791082c5467
PokemasterBill <rafsutev@outlook.com>
2026-06-23 12:25:35 +1000

    Removed f strings and added concatenation

--- Character Input.py
+++ Character Input.py
@@ -1,11 +1,11 @@
 # https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
 
 name = input("Hi, what is your name? ")
-age = int(input(f"How old are you {name}? "))
+age = int(input("How old are you " + name + "? "))
 
 current_year = 2026
 age_difference = 100 - age 
 
-final_age = current_year + age_difference
+final_age = str(current_year + age_difference)
 
-print(f"{name} will be 100 years old in the year {final_age}")
+print(name + " will be 100 years old in the year " + final_age)
