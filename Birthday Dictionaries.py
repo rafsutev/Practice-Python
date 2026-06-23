2d27d8df012a2985e081124a6ef4aba72e6610a4
PokemasterBill <rafsutev@outlook.com>
2026-06-23 14:22:40 +1000

    Complete exercise

---
+++ Birthday Dictionaries.py
@@ -0,0 +1,21 @@
+# https://www.practicepython.org/exercise/2017/01/24/33-birthday-dictionaries.html
+
+birthdays = {
+    "benjamin franklin": "01/17/1706",
+    "albert einstein": "03/14/1879",
+    "mark zuckerberg": "05/14/1984"
+}
+
+print("Welcome to the birthday dictonary here is who we have on record:")
+for key in birthdays.keys():
+    print(key.upper())
+    
+user_choice = input("Please enter a name: ").lower()
+
+if user_choice in birthdays:
+    print(f"Here is the birthday for your selection: {birthdays[user_choice]}")
+else:
+    print("Invalid option.") 
+
+
+
