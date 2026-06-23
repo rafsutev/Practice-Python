4747a38aad2ea5e598058a15f7562818ac48a7e9
PokemasterBill <rafsutev@outlook.com>
2026-06-23 22:01:52 +1000

    Complete exercise

--- Single/Write To A File.py
+++ Single/Write To A File.py
@@ -3,15 +3,17 @@
 import requests
 from bs4 import BeautifulSoup
  
-base_url = 'http://www.nytimes.com'
-r = requests.get(base_url)
-soup = BeautifulSoup(r.text)
- 
-for story_heading in soup.find_all(class_="story-heading"): 
-    if story_heading.a: 
-        print(story_heading.a.text.replace("\n", " ").strip())
-    else: 
-        print(story_heading.contents[0].strip())
+base_url = 'https://www.nytimes.com'
+r = requests.get(base_url) # GET request for the HTML from nytimes 
+soup = BeautifulSoup(r.text, "html.parser") # Take the HTML code from the nytimes website
 
-# Above code taken from solution here: https://www.practicepython.org/solution/2014/07/10/17-decode-a-web-page-solutions.html
+filename = input("File to save to: ")
 
+with open(filename, 'w') as f:
+    for story_heading in soup.find_all(class_="css-xdandi"): # Inspect element and search the headlines to find the class used 
+        if story_heading.a: 
+            f.write(story_heading.a.text.replace("\n", " ").strip())
+        else: 
+            f.write(story_heading.contents[0].get_text().strip())
+
+# Not completely my own work. Followed along with solutions: https://www.practicepython.org/solution/2014/07/10/17-decode-a-web-page-solutions.html
