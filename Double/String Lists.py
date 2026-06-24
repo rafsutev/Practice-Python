68b6c25a64f041547936525949ec4cdfedcde6e2
PokemasterBill <rafsutev@outlook.com>
2026-06-24 12:46:08 +1000

    Complete exercise

--- Double/String Lists.py
+++ Double/String Lists.py
@@ -1,15 +1,13 @@
 # https://www.practicepython.org/exercise/2014/03/12/06-string-lists.html
 
-# user_string = input("Please enter a word: ")
+user_string = input("Please enter a word: ").lower()
 
-#pasta 
-#atsap
-string = "pasta"
+def check_palindrome(string):
+    reversed = string[::-1]
+    
+    if reversed == string:
+        print("This is a palindrome")
+    else:
+        print("This is not a palindrome")
 
-empty_string = []
-
-
-
-
-
-
+check_palindrome(user_string)
