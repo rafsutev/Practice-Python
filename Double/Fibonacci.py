6c0d097ce45c4fe984b125d5c59ee67889e3766a
PokemasterBill <rafsutev@outlook.com>
2026-06-25 20:10:00 +1000

    Complete exercise

--- Double/Fibonacci.py
+++ Double/Fibonacci.py
@@ -1,5 +1,15 @@
 # https://www.practicepython.org/exercise/2014/04/30/13-fibonacci.html
 
-user_num = int(input("How many Fibonacci numbers would you like? "))
+def fibonacci(n):
+    previous = 0
+    current = 1
 
-# def fib_sequence(num):
+    for i in range(n):
+    
+        new_num = current + previous
+        print(new_num)
+    
+        current = previous
+        previous = new_num
+
+fibonacci(int(input("How many Fibonacci numbers would you like? ")))
