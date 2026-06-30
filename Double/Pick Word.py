1d4e187874734fe21b3a35ce3f865dc053afb563
PokemasterBill <rafsutev@outlook.com>
2026-06-30 11:41:08 +1000

    Saving idea for other program

--- Double/Pick Word.py
+++ Double/Pick Word.py
@@ -1,2 +1,17 @@
 # https://www.practicepython.org/exercise/2016/09/24/30-pick-word.html
 
+def check_word(length,letter):
+
+    with open("SOWPODS dictionary.txt", 'r') as f:
+        for word in f:
+            word = word.strip()
+
+            if len(word) <= length and word[0] == letter:
+                print(word)
+
+user_length = int(input("What is the length of your desired word? "))
+
+user_starting_letter = str(input("What letter does your word start with? ").upper())
+
+check_word(user_length, user_starting_letter)
+
